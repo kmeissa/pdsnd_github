0660d128ca3f4957f0e9cbb4b49adc0da69ef5e6
Kristy Moir <kristy.d.moir@leidos.com>
2021-08-31 13:26:49 +0100

    Refactoring: Updated execution order to display user stats first

--- bikeshare-submit.py
+++ bikeshare-submit.py
@@ -237,10 +237,10 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
+        user_stats(df)
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
-        user_stats(df)
         raw_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
